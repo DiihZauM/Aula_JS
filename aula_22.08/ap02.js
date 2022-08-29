d29f201dd6e47ba4d6a451d11614ba988180801b
Diogo Anjos <43689004+DiihZauM@users.noreply.github.com>
2022-08-29 20:01:07 -0300

    a

--- aula_22.08/ap02.js
+++ aula_22.08/ap02.js
@@ -1,8 +1,13 @@
-function f(){
-    var nome = 'joao';
-    function g(){
-        console.log(nome);
-    }
-    g()
-}
-f()
+const fs = require("fs");
+ const abrirArquivo = function (nomeArquivo) {
+ const exibirConteudo = function (erro, conteudo) {
+ if (erro) {
+ console.log(`Deu erro: ${erro}`);
+ } else {
+ console.log(conteudo.toString());
+ }
+ };
+fs.readFile(nomeArquivo, exibirConteudo);
+};
+
+ abrirArquivo("arquivo.txt");
