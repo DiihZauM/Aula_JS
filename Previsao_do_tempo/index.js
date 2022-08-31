4084fff3f87e52eda225352451dbb3f30ba04de1
Diogo Anjos <di.anjos84@gmail.com>
2022-08-30 21:43:21 -0300

    previsao do tempo

--- Previsao_do_tempo/index.js
+++ Previsao_do_tempo/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const axios = require ('axios')
 const appid = process.env.appid
 
-const q = 'Itu'
+const q = 'sao paulo'
 
 //metric: celsius
 //imperial: farenheit
@@ -16,9 +16,35 @@ const cnt = 10
 const base_url = 'https://api.openweathermap.org/data/2.5/forecast'
 
 const url = `${base_url}?q=${q}&units=${units}&lang=${lang}&cnt=${cnt}&appid=${appid}`
-const p = axios.get(url)
-p
-.then((res) => {
-  console.log(res.data)
-  return res.data
-})
+
+axios 
+    .get(url)
+    .then((res) => {
+        console.log(res);
+        return res.data;
+    })
+    .then((res)=> {
+        console.log(res.cnt);
+        return res;
+    })
+    .then((res)=> {
+        console.log("aqui",res);
+        return res["list"];
+    })
+    .then((res)=> {
+        for (let previsao of res) {
+        console.log(`
+        ${new Date(previsao.dt * 1000).toLocaleString()},
+        ${'Min: ' + previsao.main.temp_min}\u00B0C,
+        ${'Max: ' + previsao.main.temp_max}\u00B0C,
+        ${'Hum: ' + previsao.main.humidity}%,
+        ${previsao.weather[0].description}
+
+        `)
+        }
+        return res
+    })
+    .then((res)=> {
+    const Lista = res.filter(r => r.main.feels_like>=30)
+    console.log (`${Lista.length} previsões têm percepção humana de temperatura acima de 30 graus`)
+    });
